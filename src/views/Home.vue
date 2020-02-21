61563334027cad49cb0c67632d5dd07abad48bf8
su8ru <su8ru.i@gmail.com>
2020-02-22 04:03:25 +0900

    add station name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,30 @@
         :class="secinfo.pos"
         :secinfo="secinfo"
       ></line-section>
+      <span
+        v-for="(station, i) in stations_main"
+        :key="station"
+        :style="{ gridArea: 1 + i * 2 + '/' + 1, textAlign: 'right' }"
+        class="station-name"
+      >
+        {{ station }}
+      </span>
+      <span
+        v-for="(station, i) in stations_sagami"
+        :key="station"
+        :style="{ gridArea: 77 + i * 2 + '/' + 14 }"
+        class="station-name"
+      >
+        {{ station }}
+      </span>
+      <span
+        v-for="(station, i) in stations_takao"
+        :key="station"
+        :style="{ gridArea: 107 + i * 2 + '/' + 14 }"
+        class="station-name"
+      >
+        {{ station }}
+      </span>
     </div>
   </div>
 </template>
@@ -19,7 +43,13 @@
 .zaisen {
   display: grid;
   grid-template-rows: repeat(34, 0) repeat(calc(116 - 32), minmax(3rem, auto));
-  grid-template-columns: 5rem 1rem repeat(10, 3rem) 1rem 5rem;
+  grid-template-columns: 10rem 1rem repeat(10, 3rem) 1rem 10rem;
+
+  .line-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 }
 </style>
 
@@ -63,9 +93,7 @@ export default class Home extends Vue {
 
   async fetchData() {
     this.loading = true;
-    const resRaw = await axios.get<body>(
-      "https://i.opentidkeio.jp/data/traffic_info.json"
-    );
+    const resRaw = await axios.get<body>("/traffic_info_sample.json");
 
     if (!resRaw.data) return;
     const response: body = resRaw.data;
@@ -109,5 +137,76 @@ export default class Home extends Vue {
 
     this.loading = false;
   }
+
+  stations_main = [
+    "本八幡",
+    "篠崎",
+    "瑞江",
+    "一之江",
+    "船堀",
+    "東大島",
+    "大島",
+    "西大島",
+    "住吉",
+    "菊川",
+    "森下",
+    "浜町",
+    "馬喰横山",
+    "岩本町",
+    "小川町",
+    "神保町",
+    "九段下",
+    "市ヶ谷",
+    "曙橋",
+    "新宿三丁目",
+    "新宿",
+    "初台",
+    "幡ヶ谷",
+    "笹塚",
+    "代田橋",
+    "明大前",
+    "下高井戸",
+    "桜上水",
+    "上北沢",
+    "八幡山",
+    "芦花公園",
+    "千歳烏山",
+    "仙川",
+    "つつじヶ丘",
+    "柴崎",
+    "国領",
+    "布田",
+    "調布",
+    "西調布",
+    "飛田給",
+    "武蔵野台",
+    "多磨霊園",
+    "東府中",
+    "府中",
+    "分倍河原",
+    "中河原",
+    "聖蹟桜ヶ丘",
+    "百草園",
+    "高幡不動",
+    "南平",
+    "平山城址公園",
+    "長沼",
+    "北野",
+    "京王八王子 "
+  ];
+  stations_sagami = [
+    "京王多摩川",
+    "京王稲田堤",
+    "京王よみうりランド",
+    "稲城",
+    "若葉台",
+    "京王永山",
+    "京王多摩センター",
+    "京王堀之内",
+    "南大沢",
+    "多摩境",
+    "橋本"
+  ];
+  stations_taako = ["京王片倉", "山田", "めじろ台", "狭間", "高尾", "高尾山口"];
 }
 </script>
