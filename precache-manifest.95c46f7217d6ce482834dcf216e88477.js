bf890e0f81283140b44b5def4c86ede80f65762a
su8ru <su8ru@users.noreply.github.com>
2020-02-22 18:35:14 +0000

    Automated deployment: Sat Feb 22 18:35:14 UTC 2020 87fbc74038fc094423be39aabe7413472e0e6484

---
+++ precache-manifest.95c46f7217d6ce482834dcf216e88477.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed46ff9d1827095a879",
+    "url": "css/app.ebc9ffd2.css"
+  },
+  {
+    "revision": "234904905c161f47cf1d",
+    "url": "css/home.155af81c.css"
+  },
+  {
+    "revision": "dffc4d431fbe93ef608ed54c90f78f85",
+    "url": "index.html"
+  },
+  {
+    "revision": "9ed46ff9d1827095a879",
+    "url": "js/app.e5bf256f.js"
+  },
+  {
+    "revision": "43c6cfe8efc0e08cdeb5",
+    "url": "js/chunk-vendors.55544758.js"
+  },
+  {
+    "revision": "234904905c161f47cf1d",
+    "url": "js/home.3ec9be15.js"
+  },
+  {
+    "revision": "6427289e265a3624b811b64928e65237",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "1064f4e9c92f56f4077242ca62888408",
+    "url": "traffic_info.json"
+  }
+]);
